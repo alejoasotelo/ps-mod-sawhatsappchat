63eea3fc383ab9a7aff16d129661f8c1902d735f
Alejo Sotelo <alesotelo@gmail.com>
2024-08-28 18:49:11 -0300

    [*] Ejecuté header-stamp y php-cs-fixer

--- sawhatsappchat.php
+++ sawhatsappchat.php
@@ -1,29 +1,18 @@
 <?php
 /**
-* 2007-2024 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2024 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
-
+ * Copyright since 2007 Alejo A. Sotelo <alejosotelo.com.ar>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a commercial license from Alejo A. Sotelo
+ * Use, copy, modification or distribution of this source file without written
+ * license agreement from Alejo A. Sotelo is strictly forbidden.
+ * In order to obtain a license, please contact us: soporte@alejosotelo.com.ar
+ *
+ * @author    Alejo A. Sotelo <soporte@alejosotelo.com.ar>
+ * @copyright Copyright (c) since 2007 Alejo Sotelo
+ * @license   Commercial License
+ */
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -40,7 +29,7 @@ class Sawhatsappchat extends Module
         $this->author = 'Alejo Sotelo <alejosotelo.com.ar>';
         $this->need_instance = 0;
 
-        /**
+        /*
          * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
          */
         $this->bootstrap = true;
@@ -50,7 +39,7 @@ class Sawhatsappchat extends Module
         $this->displayName = $this->l('SA Whatsapp Chat');
         $this->description = $this->l('Este módulo agrega un botón de Whatsapp a tu tienda para que tus usuarios inicien un chat con tu empresa.');
 
-        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
     }
 
     /**
@@ -79,18 +68,18 @@ class Sawhatsappchat extends Module
      */
     public function getContent()
     {
-        /**
+        /*
          * If values have been submitted in the form, process.
          */
-        if (((bool)Tools::isSubmit('submitSawhatsappchatModule')) == true) {
+        if (((bool) Tools::isSubmit('submitSawhatsappchatModule')) == true) {
             $this->postProcess();
         }
 
         $this->context->smarty->assign('module_dir', $this->_path);
 
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
 
-        return $output.$this->renderForm();
+        return $output . $this->renderForm();
     }
 
     /**
@@ -109,16 +98,16 @@ class Sawhatsappchat extends Module
         $helper->identifier = $this->identifier;
         $helper->submit_action = 'submitSawhatsappchatModule';
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
 
-        $helper->tpl_vars = array(
+        $helper->tpl_vars = [
             'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
             'languages' => $this->context->controller->getLanguages(),
             'id_language' => $this->context->language->id,
-        );
+        ];
 
-        return $helper->generateForm(array($this->getConfigForm()));
+        return $helper->generateForm([$this->getConfigForm()]);
     }
 
     /**
@@ -126,13 +115,13 @@ class Sawhatsappchat extends Module
      */
     protected function getConfigForm()
     {
-        return array(
-            'form' => array(
-                'legend' => array(
+        return [
+            'form' => [
+                'legend' => [
                 'title' => $this->l('Settings'),
                 'icon' => 'icon-cogs',
-                ),
-                'input' => array(
+                ],
+                'input' => [
                     // array(
                     //     'type' => 'switch',
                     //     'label' => $this->l('Live mode'),
@@ -152,7 +141,7 @@ class Sawhatsappchat extends Module
                     //         )
                     //     ),
                     // ),
-                    array(
+                    [
                         'col' => 3,
                         'type' => 'text',
                         'prefix' => '<i class="icon icon-whatsapp"></i>',
@@ -160,8 +149,8 @@ class Sawhatsappchat extends Module
                         'name' => 'SAWHATSAPPCHAT_PHONE',
                         'label' => $this->l('Whatsapp Phone'),
                         'placeholder' => '5491123456789',
-                    ),
-                    array(
+                    ],
+                    [
                         'col' => 3,
                         'type' => 'text',
                         'prefix' => '<i class="icon icon-text"></i>',
@@ -169,13 +158,13 @@ class Sawhatsappchat extends Module
                         'name' => 'SAWHATSAPPCHAT_MESSAGE',
                         'label' => $this->l('Whatsapp Message'),
                         'placeholder' => 'Hola, estoy en %s',
-                    ),
-                ),
-                'submit' => array(
+                    ],
+                ],
+                'submit' => [
                     'title' => $this->l('Save'),
-                ),
-            ),
-        );
+                ],
+            ],
+        ];
     }
 
     /**
@@ -183,10 +172,10 @@ class Sawhatsappchat extends Module
      */
     protected function getConfigFormValues()
     {
-        return array(
+        return [
             'SAWHATSAPPCHAT_PHONE' => Configuration::get('SAWHATSAPPCHAT_PHONE', null, null, null, ''),
             'SAWHATSAPPCHAT_MESSAGE' => Configuration::get('SAWHATSAPPCHAT_MESSAGE', null, null, null, ''),
-        );
+        ];
     }
 
     /**
@@ -204,11 +193,11 @@ class Sawhatsappchat extends Module
     public function hookDisplayHeader()
     {
         // path in module views/img/whastapp-48x48.jpg
-        $whasappLogo = $this->_path.'views/img/whastapp-48x48.jpg';
+        $whasappLogo = $this->_path . 'views/img/whastapp-48x48.jpg';
 
         // if user request support webp format use it
         if (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) {
-            $whasappLogo = $this->_path.'views/img/whastapp-48x48.webp';
+            $whasappLogo = $this->_path . 'views/img/whastapp-48x48.webp';
         }
 
         Media::addJsDef([
@@ -220,22 +209,22 @@ class Sawhatsappchat extends Module
         ]);
 
         $this->context->controller->registerStylesheet(
-            'module-sawhatsappchat', 
-            'modules/'.$this->name.'/views/css/front.css', 
+            'module-sawhatsappchat',
+            'modules/' . $this->name . '/views/css/front.css',
             [
-                'media' => 'all', 
+                'media' => 'all',
                 'priority' => 150,
-                'version' => $this->version
+                'version' => $this->version,
             ]
         );
 
         $this->context->controller->registerJavascript(
-            'module-sawhatsappchat', 
-            'modules/'.$this->name.'/views/js/front.js', 
+            'module-sawhatsappchat',
+            'modules/' . $this->name . '/views/js/front.js',
             [
-                'position' => 'bottom', 
+                'position' => 'bottom',
                 'priority' => 150,
-                'version' => $this->version
+                'version' => $this->version,
             ]
         );
     }
