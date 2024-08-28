afdf0860db61f9d5928664a03fd17826361b7cf8
Alejo Sotelo <alesotelo@gmail.com>
2024-08-28 18:46:02 -0300

    [+] Agregué test para phpstan.

---
+++ tests/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright since 2007 Alejo A. Sotelo <alejosotelo.com.ar>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a commercial license from Alejo A. Sotelo
+ * Use, copy, modification or distribution of this source file without written
+ * license agreement from Alejo A. Sotelo is strictly forbidden.
+ * In order to obtain a license, please contact us: soporte@alejosotelo.com.ar
+ *
+ * @author    Alejo A. Sotelo <soporte@alejosotelo.com.ar>
+ * @copyright Copyright (c) since 2007 Alejo Sotelo
+ * @license   Commercial License
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
